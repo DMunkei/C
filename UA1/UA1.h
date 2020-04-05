6dfd508d77ab4837b72ff82539fb1af46a00b68e
DMunkei <stareye863@gmail.com>
2020-04-05 23:03:46 +0200

    Restrutctured the directories and the place where main.c is actually located. All Weekly tasks will be in their respective directories and will be included into main.c to test" When importing functionality f rom one file to another, import them using HEADERS! Don't import the file directly, it will break the project/not compile. How to do this properly: 1. #include "current_file_name.h" 2.write down your functionalities just as you would. 3.create a HEADER file with the same name file_name.c and then just write the function declarations. 4.Import/Include the file_name.h in the file you want to import that functionality into!"

---
+++ UA1/UA1.h
@@ -0,0 +1,8 @@
+//
+// Created by domin on 05/04/2020.
+//
+
+#ifndef C_UA1_H
+#define C_UA1_H
+
+#endif //C_UA1_H
---
+++ UA2/UA2.h
@@ -0,0 +1,8 @@
+//
+// Created by domin on 05/04/2020.
+//
+
+#ifndef C_UA2_H
+#define C_UA2_H
+
+#endif //C_UA2_H
